bd76be642baaf8c8c241f488eeb15a3f65090b4e
duongthenho <69722419+duongthenho@users.noreply.github.com>
2021-06-08 21:53:23 +0700

    Update create_table.sql

--- database/create_table.sql
+++ database/create_table.sql
@@ -1,5 +1,7 @@
 use newservlet12month2018;
 
+use webdonghothoitrang
+
 create table role(
 	id bigint not null primary key auto_increment,
 	name VARCHAR(255) NOT NULL,
@@ -14,7 +16,10 @@ CREATE TABLE user (
   id bigint NOT NULL PRIMARY KEY auto_increment,
   username VARCHAR(150) NOT NULL,
   password VARCHAR(150) NOT NULL,
+  fullname VARCHAR(150) NULL,
   status int NOT NULL,
+  numberphone VARCHAR(150) NULL,
+  email VARCHAR(150) NULL,
   roleid bigint NOT NULL,
   createddate TIMESTAMP NULL,
   modifieddate TIMESTAMP NULL,
@@ -22,22 +27,6 @@ CREATE TABLE user (
   modifiedby VARCHAR(255) NULL
 );
 ALTER TABLE user ADD CONSTRAINT fk_user_role FOREIGN KEY (roleid) REFERENCES role(id);
-
-Create table userinfor(
-  id bigint NOT NULL PRIMARY KEY auto_increment,
-  fullname varchar(150) not null,
-  address varchar(255) not null,
-  numberphone varchar(150) not null,
-  email varchar(150) not null,
-  note varchar(255),
-  userid bigint not null,
-  createddate TIMESTAMP NULL,
-  modifieddate TIMESTAMP NULL,
-  createdby VARCHAR(255) NULL,
-  modifiedby VARCHAR(255) NULL
-);
-ALTER TABLE userinfor ADD CONSTRAINT fk_userinfor_user FOREIGN KEY (userid) REFERENCES user(id);
-
 
 CREATE TABLE category (
   id bigint NOT NULL PRIMARY KEY auto_increment,
@@ -64,39 +53,20 @@ create table product(
 );
 ALTER TABLE product ADD CONSTRAINT fk_product_category FOREIGN KEY (categoryid) REFERENCES category(id);
 
-create table bill(
-	id bigint NOT NULL PRIMARY KEY auto_increment,
-	totalmoney VARCHAR(255) not null,
-	status VARCHAR(255) not null,
-	userinforid bigint NOT NULL,
-    createddate TIMESTAMP NULL,
-	modifieddate TIMESTAMP NULL,
-	createdby VARCHAR(255) NULL,
-	modifiedby VARCHAR(255) NULL
-);
-ALTER TABLE bill ADD CONSTRAINT fk_bill_userinfor FOREIGN KEY (userinforid) REFERENCES userinfor(id);
-
-create table detailbill(
-  id bigint NOT NULL PRIMARY KEY auto_increment,
-  productid bigint NOT NULL,
-  price float not null,
-  amount int not null,
-  billid bigint not null,
-  createddate TIMESTAMP NULL,
+create table cart(
+ id bigint NOT NULL PRIMARY KEY auto_increment,
+ product_id bigint NOT NULL,
+ user_id bigint not null,
+ amount int not null,
+ createddate TIMESTAMP NULL,
   modifieddate TIMESTAMP NULL,
   createdby VARCHAR(255) NULL,
-  modifiedby VARCHAR(255) NULL
+  modifiedby VARCHAR(255) NULL,
+  billid bigint not null
 );
-ALTER TABLE detailbill ADD CONSTRAINT fk_detailbill_product FOREIGN KEY (productid) REFERENCES product(id);
-ALTER TABLE detailbill ADD CONSTRAINT fk_detailbill_bill FOREIGN KEY (billid) REFERENCES bill(id);
 
-
-create table quangcao(
-	id bigint NOT NULL PRIMARY KEY auto_increment,
-	image1 longblob,
-	image2 longblob,
-	image3 longblob
-);
+ALTER TABLE cart ADD CONSTRAINT fk_cart_product FOREIGN KEY (product_id) REFERENCES product(id);
+ALTER TABLE cart ADD CONSTRAINT fk_cart_user FOREIGN KEY (user_id) REFERENCES user(id);
 
 create table tinhthanhpho(
  matp VARCHAR(255) not null primary key,
@@ -119,6 +89,29 @@ create table xaphuongthitran(
  maqh VARCHAR(255) not null
 );
 ALTER TABLE xaphuongthitran ADD CONSTRAINT fk_xaphuongthitran_quanhuyen FOREIGN KEY (maqh) REFERENCES quanhuyen(maqh);
+
+create table quangcao(
+	id bigint NOT NULL PRIMARY KEY auto_increment,
+	image1 longblob,
+	image2 longblob,
+	image3 longblob
+);
+
+create table bill(
+	id bigint NOT NULL PRIMARY KEY auto_increment,
+	fullname VARCHAR(255) not null,
+	numberphone VARCHAR(255) not null,
+	email VARCHAR(255) not null,
+	address VARCHAR(255) not null,
+	totalmoney VARCHAR(255) not null,
+	status VARCHAR(255) not null,
+	user_id bigint NOT NULL,
+        createddate TIMESTAMP NULL,
+	modifieddate TIMESTAMP NULL,
+	createdby VARCHAR(255) NULL,
+	modifiedby VARCHAR(255) NULL
+);
+ALTER TABLE bill ADD CONSTRAINT fk_bill_user FOREIGN KEY (user_id) REFERENCES user(id);
 
 
 
